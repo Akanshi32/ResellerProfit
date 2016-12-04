bcd4db891c8804f2bce4ee5d655d78463e72f2fc
AKANSHI SRIVASTAVA <AKANSHI SRIVASTAVA>
2016-12-04 19:56:49 +0530

    DialogAlert

--- app/src/main/java/com/vansh/resellerprofit/MainActivity.java
+++ app/src/main/java/com/vansh/resellerprofit/MainActivity.java
@@ -1,8 +1,13 @@
 package com.vansh.resellerprofit;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Point;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.view.Display;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,16 +17,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    int width,height;
+    Button cancel,submit;
+    ImageView addButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+        height = size.y;
+        width = size.x;
+
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -40,7 +60,28 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+        openDialog();
+
+        ImageView addButton = (ImageView) findViewById(R.id.imageView2);
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openDialog();
+            }
+        });
+
+
     }
+    public void openDialog(){
+        final Dialog dialog = new Dialog(this);
+        dialog.setContentView(R.layout.dialog);
+        Button cancel=(Button) findViewById(R.id.dialog_cancel);
+        Button submit=(Button) findViewById(R.id.dialog_submit);
+        dialog.setTitle("Product Details");
+        dialog.getWindow().setLayout(width*90/100, height*50/100);
+        dialog.show();
+
+           }
 
     @Override
     public void onBackPressed() {
@@ -98,4 +139,5 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
 }
